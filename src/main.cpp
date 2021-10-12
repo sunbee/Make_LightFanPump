55e8f1ddaad7ba25dc2f5c26451178911d4a6c8f
sunbee <sanjay.bhatikar@gmail.com>
2021-10-12 09:58:31 -0500

    Updated main to run tests only.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 #include "Pins.h"
 #include "LED.h"
+#include "Fan.h"
 
 LED Light;
+Fan FanPC;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,15 +16,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // cycle();
-  Light.set_brightness(55);
-  Serial.print("Started cycling colors .. ");
-  Light.cycle_fade();
-  Serial.print("Complete! \nStarting white light .. ");
-  Light.switch_on();
-  delay(3000);
-  Serial.print("Complete! \nPowering down  .. ");
-  Light.switch_off();
-  delay(1000);
-  Serial.print("Complete! \nResume normal operation.");
-  Serial.println();
+  Light.test_me();
+  FanPC.test_me();
 }
